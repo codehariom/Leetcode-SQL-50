9cb3e3c204f06588eaea8e58bc18c292899e034e
Hariom Gupta <digitalwithhariom@gmail.com>
2025-03-22 12:38:53 +0530

    Create Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Basic Joins/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,35 @@
+/* Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+
+Table: Transactions
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.*/
+
+Code
+
+select visits.customer_id , count(customer_id) as count_no_trans
+from visits left join Transactions on visits.visit_id = Transactions.visit_id
+where Transactions.transaction_id is Null
+group by  visits.customer_id
